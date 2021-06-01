c199fe7640a8bbc186b773e2696de7508d3f1a5a
Ritik Shah <ritik10.shah@gmail.com>
2021-06-01 04:04:24 -0700

    rx: fixed up scoreboards-pandas

--- scoreboard-pandas/main.py
+++ scoreboard-pandas/main.py
@@ -1,16 +1,38 @@
 import pandas as pd
 import nbtlib
+import time
 
-nbt_file = nbtlib.load('scoreboard.dat')
-scores = pd.DataFrame()
-objectives = pd.Series(dtype=str)
+try:
+    import rich
+    RICH = True
+except ImportError:
+    RICH = False
 
-for score in nbt_file.root['data']['PlayerScores']:
-    scores.loc[str(score['Objective']), str(score['Name'])] = int(score['Score'])
 
-scores.to_csv('scoreboard_scores.csv')
+def print_helper(color, text):
+    if RICH:
+        rich.print(color + text)
+    else:
+        print(text)
 
-for obj in nbt_file.root['data']['Objectives']:
-    objectives[str(obj['Name'])] = str(obj['CriteriaName'])
 
-objectives.to_csv('scoreboard_objectives.csv')
+t0 = time.perf_counter()
+print_helper('[blue]', 'Loading in data..')
+
+nbt_file = nbtlib.load('scoreboard.dat').root['data'].unpack()
+scores = pd.DataFrame(columns=['Objective', 'Player', 'Score'])
+objectives = pd.DataFrame(columns=['Objective', 'Type'])
+
+print_helper('[blue]', 'Getting scores...')
+scores = pd.DataFrame(nbt_file['PlayerScores'])
+print_helper('[green]', '  Saving to csv')
+scores.to_csv('scoreboard_scores.csv', index=False)
+
+print_helper('[blue]', 'Getting objectives...')
+objectives = pd.DataFrame(nbt_file['Objectives'])
+print_helper('[green]', '  Saving to csv')
+
+objectives.to_csv('scoreboard_objectives.csv', index=False)
+
+elapsed = time.perf_counter() - t0
+print_helper('[bold green]', f'Done in {elapsed:.3f}s')
